2f13433c945ad1394b56dc8358590c4fafe73685
Anastasiia Salenko <batsvinova@gmail.com>
2022-01-08 18:50:59 +0200

    Initial commit: setup of webpack

---
+++ src/create-new-note.js
@@ -0,0 +1,3 @@
+export const createNewNote = () => {
+  console.log('I have a dog');
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createNewNote } from './create-new-note';
+import './main.css';
+
+console.log('I have a cat');
+
+createNewNote();
